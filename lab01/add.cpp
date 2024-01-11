7668cb5976e35ee7b4cdcfdbbd06a2daaa03c7eb
rsaini17 <m275580@usna.edu>
2024-01-11 09:30:53 -0500

    msg

---
+++ lab01/add.cpp
@@ -0,0 +1,15 @@
+// (Ronny Saini m275580)
+// This program adds two numbers
+
+#include <iostream>
+
+int main()
+{
+  int number1, number2, sum;
+  number1 = 12;
+  number2 = 13;
+  sum = number1 + number2;
+  std::cout << "The sum of these two integers is "
+            << sum << std::endl;
+  return 0;
+}
