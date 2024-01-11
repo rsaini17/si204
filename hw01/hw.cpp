6c1696f213f88994cd1f7894737c54ae0b148f77
rsaini17 <m275580@usna.edu>
2024-01-11 14:15:16 -0500

    msg

--- hw01/hw.cpp
+++ hw01/hw.cpp
@@ -3,20 +3,21 @@
 
 using namespace std;
 
-
 int main()
 {
   double x, y, z;
   cout << "Enter length x: ";
   cin >> x;
-  cout << "Enter length y: ";
+  cout << "Enter length y (<" << x << "): ";
   cin >> y;
-  cout << "Enter length z: ";
+  double ac = sqrt(x*x-y*y);
+  cout << "Enter length z (<" << ac << "): ";
   cin >> z;
 
-  double ac = sqrt(x*x-y*y);
   double dc = ac - z;
-  double bd = sqrt(
+  double bd = sqrt((dc*dc)+(y*y));
+
+  cout << "Length of BD is " << bd <<endl;
 
   return 0;
 }
