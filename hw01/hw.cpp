60ce933776b50ca44a56eb9757389af86cf4b221
rsaini17 <m275580@usna.edu>
2024-01-11 09:45:32 -0500

    msg

---
+++ hw01/hw.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+  double x, y, z;
+  cout << "Enter length x: ";
+  cin >> x;
+  cout << "Enter length y: ";
+  cin >> y;
+  cout << "Enter length z: ";
+  cin >> z;
+
+  double ac = sqrt(x*x-y*y);
+  double dc = ac - z;
+  double bd = sqrt(
+
+  return 0;
+}
