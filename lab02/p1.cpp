c24991b83e0865148c01051dfcd112bd6ec9b9a4
rsaini17 <m275580@usna.edu>
2024-01-18 09:10:06 -0500

    msg

---
+++ lab02/p1.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+  //declare variables
+  char a,b,c,d;
+  int total = 0;
+
+  //ask user for input
+  cout << "Enter a 4-bit binary number: ";
+  cin >> a >> b >> c >> d;
+
+  //calculate total by finding sum of all bases and digits
+  
+  total = (a-48)*8 + (b-48)*4 + (c-48)*2 + (d-48)*1;
+
+  cout << "In decimal " << a << b << c << d << " = " << total << endl;
+  
+  //function return
+  return 0;
+}
