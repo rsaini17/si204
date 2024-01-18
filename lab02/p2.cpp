811ac29a0a96c8a113d7f27fff0a9d1335580f71
rsaini17 <m275580@usna.edu>
2024-01-18 09:38:38 -0500

    msg

---
+++ lab02/p2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  //declare variables
+  int x,a,b,c,d;
+
+  //ask user for input
+  cout << "Enter a number between 0 and 15: ";
+  cin >> x;
+
+  //repeat 4 times:mod2 to see if base present to get digit, then divide by 2. Reverse quotients at the end. 
+  a = x%2;
+  b = (x/2)%2;
+  c = ((x/2)/2)%2;
+  d = (((x/2)/2)/2)%2;
+  
+  //print to user
+  cout << x << " in binary is " << d << c << b << a << endl;  
+
+  //function return
+  return 0;
+}
---
+++ lab02/p3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  char a,b,c;
+
+  //get input from user
+  cout << "Permissions: ";
+  cin >> a >> b >> c;
+
+  //User:
+
+  
+  //return for function
+  return 0;
+}
+
