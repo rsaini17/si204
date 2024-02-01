f9e987efcc4cf96703be993409ceae6b742e3991
rsaini17 <m275580@usna.edu>
2024-02-01 08:29:17 -0500

    msg

--- lab04/part1.cpp
+++ lab04/part1.cpp
@@ -8,7 +8,32 @@ using namespace std;
 
 int main(){
   
+  //opening file name
+  string filename;
+  cin >> filename;
+  ifstream fin(filename);
+  
+  //check to make sure file exists
+  if (!fin){
+    cout << "Could not open file '" << filename << "'" << endl;
+    return 1;
+  }
 
+  //declare variables
+  string junk;
+  double temp, total, count = 0;
+  fin >> junk > junk;
+  
+  while (fin > junk > temp){
+    total = total + temp;
+    count++;
+  }
 
+  //find avg and then convert to F
+  total = total/avg;
+  total = (9/5)*total + 32;
 
+  cout << "file: " << filename << endl << "ave: " << total << endl;
+
+  return 0;
 }
