60e4d45ef3474606ec0075af9197824c6afc0894
rsaini17 <m275580@usna.edu>
2024-02-22 08:48:43 -0500

    msg

---
+++ lab06/p3.cpp
@@ -0,0 +1,79 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+// Function to roll die
+int rolldie() {
+    int roll;
+    bool notValid = true;
+    while (notValid) {
+        roll = rand() % 8;
+        if (roll >= 1 && roll <= 6){
+            notValid = false;
+        }
+    }
+    return roll;
+}
+
+// Function to simulate a single throw of two dice and process the role within the game
+int throwdice(int& setpoint) {
+    int roll1 = rolldie();
+    int roll2 = rolldie();
+    int sum = roll1 + roll2;
+
+    cout << "Player rolled " << roll1 << " + " << roll2 << " = " << sum;
+
+    if (setpoint == 0) { // First roll
+        if (sum == 7 || sum == 11){
+            cout << " Player wins!" << endl;
+            return 0;
+        }
+        else if (sum == 2 || sum == 3 || sum == 12) {
+            cout << " House wins!" << endl;
+            return -1;
+        }
+        else {
+            setpoint = sum;
+            cout << " setpoint is " << setpoint << "!" << endl;
+        }
+    } else { // Subsequent rolls
+        if (sum == setpoint) {
+            cout << " Player wins!" << endl;
+            return 0;
+        } else if (sum == 7 || sum == 12) {
+            cout << " House wins!" << endl;
+            return -1;
+        } else {
+            cout << " roll again" << endl;
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+    // Seed
+    int seed;
+    char playAgain;
+    while (true) {
+        cout << "Enter seed value: ";
+        cin >> seed;
+        srand(seed);
+
+        // Play Craps
+        int setpoint = 0;
+        int result;
+        while (true) {
+            result = throwdice(setpoint);
+            if (result == 0 || result == -1)
+                break;
+        }
+
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+        if (playAgain != 'y' && playAgain != 'Y')
+            break;
+    }
+
+    return 0;
+}
